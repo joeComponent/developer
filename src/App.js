7a571e59192e352aa39f8573fbe59eb9e396b678
chrbar <chris.barteau@oregon.gov>
2019-05-24 15:29:52 -0700

    Initial checkin

--- src/App.js
+++ src/App.js
@@ -10,14 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+		<p>and... i just did that!</p>
+        <a className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer" >Learn React</a>
       </header>
     </div>
   );
