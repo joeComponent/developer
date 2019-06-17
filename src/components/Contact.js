07a39605d853361e01f92d4e4da09a476fdb2c4d
chrbar <chris.barteau@oregon.gov>
2019-06-17 15:07:27 -0700

    Contact javascript

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,11 @@
 import React from 'react';
+import { emailjs } from 'emailjs-com/dist/email.min';
 
 const Contact = () => {
 
-    handleSubmit (evnt) {
-        console.log('BEGIN: handleSubmit');
-        evnt.preventDefault();
-
-        const {
-            REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-            REACT_APP_EMAILJS_TEMPLATEID: template
-        } = this.props.env;
-
-        this.sendFeedback( template,
-            this.props.senderEmail,
-            receiverEmail,
-            this.state.msg);
-        
-        this.setState({ formSubmitted: true });
-        console.log('END: handleSubmit');
-    }
-
-    handleChange (evnt) {
-        console.log('BEGIN: handleChange');
-
-        console.log('END: handleChange');
-    }
-
-    sendFeedback (templateId, senderEmail, receiverEmail, msg) {
-        console.log('BEGIN: sendFeedback');
-
-        console.log('END: sendFeedback');
-    }
-
     return (
         <div className="">
-            <form onSubmit={handleSubmit}>
+            <form id='contactFrm' onSubmit={handleSubmit}>
                 <div className="card bg-white border-dark my-5">
                     <div className="card-header">
                         <h1 className="card-title">Contact</h1>
@@ -45,7 +16,7 @@ const Contact = () => {
                         <div className="row mx-auto">
                             <div id="firstNmFld" className="col-4">
                                 <label htmlFor="firstName" className="col-form-label">First Name</label>
-                                <input id="firstName" className="form-control form-control-sm" />
+                                <input id="firstName" className="form-control form-control-sm"  required/>
                             </div>
                             <div id="lastNmFld" className="col-4">
                                 <label htmlFor="lastNm" className="col-form-label">Last Name</label>
@@ -58,12 +29,11 @@ const Contact = () => {
                         </div>
                         <div id="emailFld" className="col-12">
                             <label htmlFor="email" className="col-form-label">Email Address</label>
-                            <input id="email" type="text" className="form-control form-control-sm" />
+                            <input id="email" name="email" type="text" className="form-control form-control-sm"  required/>
                         </div>
                         <div id="msgFld" className="col-12">
                             <label htmlFor="msg-entry" className="col-form-label">Message</label>
-                            <textarea id="msg-entry" name="msg-entry" className="form-control form-control-sm"
-                                placeholder="Enter your message here" required onChange={handleChange} value={msg} />
+                            <textarea id="msg" className="form-control form-control-sm" placeholder="Enter your message here" required />
                         </div>
                     </div>
                     <div className="card-footer text-right">
@@ -73,6 +43,48 @@ const Contact = () => {
             </form>
         </div>
     );
+}
+
+function handleSubmit (evnt) {
+    evnt.preventDefault();
+    console.log('BEGIN: handleSubmit');
+    var form = document.getElementById("contactFrm");
+
+    sendMsg(form.firstName.value,
+            form.lastNm.value,
+            form.jobNm.value,
+            form.email.value,
+            form.msg.value);
+    
+    console.log('END: handleSubmit');
+}
+
+function sendMsg (fNm, lNm, jNm, email, msg) {
+    var tid = process.env.REACT_APP_EMAILJS_TEMPLATEID;
+    var sid = process.env.REACT_APP_EMAILJS_SERVICEID;
+    var uid = process.env.REACT_APP_EMAILJS_USERID;
+
+    console.log('BEGIN: sendMsg');
+    console.log("tid: " + tid + "  uid: " + uid);
+    console.log("msg: " + msg + "  email: " + email);
+
+    var tparms = {
+        f_nm: fNm,
+        l_nm: lNm,
+        j_nm: jNm,
+        message: msg
+    };
+
+    //Their ex.:
+    // emailjs.send(sid, tid, tparms, uid)
+    // .then(function(response) {
+        // console.log('SUCCESS!', response.status, response.text);
+        //todo - ??? this.setState({formEmailSent: true})
+    //  }, function(error) {
+        // console.log('FAILED...', error);
+    //  });
+
+    console.log('END: sendMsg');
 }
 
 export default Contact;
