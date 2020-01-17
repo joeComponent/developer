ae96f1e0bab7398b3b7f1fcb02e0f01a24012904
Chris Barteau <chrbar@pop-os.localdomain>
2020-01-17 09:54:57 -0800

    Updates

---
+++ precache-manifest.610997b529303dcb8c4a6f0b8f37fed0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571d964e1dd8ebaa73f46547776871ed",
+    "url": "/developer/index.html"
+  },
+  {
+    "revision": "5f0e10b9a00826efeec2",
+    "url": "/developer/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "ce1b1d98078927580439",
+    "url": "/developer/static/css/main.9d9503f2.chunk.css"
+  },
+  {
+    "revision": "5f0e10b9a00826efeec2",
+    "url": "/developer/static/js/2.7ea91042.chunk.js"
+  },
+  {
+    "revision": "ce1b1d98078927580439",
+    "url": "/developer/static/js/main.163c6340.chunk.js"
+  },
+  {
+    "revision": "97abc3a5f2ddcb309763",
+    "url": "/developer/static/js/runtime~main.64bdd9f2.js"
+  },
+  {
+    "revision": "bd9d8c689c210e6664b72d0d7d5eb5de",
+    "url": "/developer/static/media/CHLA.bd9d8c68.jpg"
+  },
+  {
+    "revision": "0a6a8de2a59195ac392ed2f4fe81c131",
+    "url": "/developer/static/media/CJIS.0a6a8de2.png"
+  },
+  {
+    "revision": "a05a0ab709f33b258a633377aef434ea",
+    "url": "/developer/static/media/CUDenver.a05a0ab7.png"
+  },
+  {
+    "revision": "abc4b729276faa5243a4c3b4e4ed49b4",
+    "url": "/developer/static/media/FOP.abc4b729.png"
+  },
+  {
+    "revision": "be536308a02cc3d972cda8e1adb3c431",
+    "url": "/developer/static/media/GWU.be536308.png"
+  },
+  {
+    "revision": "77874e8e522184eaf0d7c13edb0fa98e",
+    "url": "/developer/static/media/HarrisSwitch.77874e8e.png"
+  },
+  {
+    "revision": "b23cf0127644d9ebfdd8c40fd5d29d0b",
+    "url": "/developer/static/media/Html5.b23cf012.png"
+  },
+  {
+    "revision": "b92d38dfc536f59835c80ed8bd31fed7",
+    "url": "/developer/static/media/InstitutFr.b92d38df.png"
+  },
+  {
+    "revision": "347dd7d98002f899892cbbaf09dc41c6",
+    "url": "/developer/static/media/ItilCert.347dd7d9.png"
+  },
+  {
+    "revision": "226f1562d265b4835bc30d65492a5a5a",
+    "url": "/developer/static/media/Java.226f1562.png"
+  },
+  {
+    "revision": "2198f7b4ae2e476a9b89140bffbee2f0",
+    "url": "/developer/static/media/KrugerNP.2198f7b4.png"
+  },
+  {
+    "revision": "b6a058d3f9b2a99643c69b51f0f2fdd0",
+    "url": "/developer/static/media/Laboratory.b6a058d3.png"
+  },
+  {
+    "revision": "f0243006df50bcd0c0bc410bbb9a851f",
+    "url": "/developer/static/media/OSU.f0243006.png"
+  },
+  {
+    "revision": "24b3c5e446d89d74b3376f4ed6a138e9",
+    "url": "/developer/static/media/OWASP.24b3c5e4.png"
+  },
+  {
+    "revision": "f713fea0a41015fcb36b6e9b387e1f58",
+    "url": "/developer/static/media/PayPal.f713fea0.png"
+  },
+  {
+    "revision": "0d7303d5f63738ec38f21465017458d6",
+    "url": "/developer/static/media/SC.0d7303d5.png"
+  },
+  {
+    "revision": "b512c615c34aa28c4037645775baff90",
+    "url": "/developer/static/media/SPSS.b512c615.png"
+  },
+  {
+    "revision": "5a640f5c70fd1f8184a79d2033f26773",
+    "url": "/developer/static/media/SosReview18.5a640f5c.png"
+  },
+  {
+    "revision": "1926e3625834992f5f97293d5d4764f1",
+    "url": "/developer/static/media/StudioDesk.1926e362.jpg"
+  },
+  {
+    "revision": "ddfbfe0614339ebf25bcdbf2f23b479f",
+    "url": "/developer/static/media/StudioDesk2.ddfbfe06.jpg"
+  },
+  {
+    "revision": "c5ea719efc73fee0cd994b5a9b5b81e1",
+    "url": "/developer/static/media/StudioDesk3.c5ea719e.jpg"
+  },
+  {
+    "revision": "49646c6dc1c5053e2b0549c4fa393a91",
+    "url": "/developer/static/media/dmhmms.49646c6d.png"
+  },
+  {
+    "revision": "c9f33a6fc6b39600e5d85008f8d42f5b",
+    "url": "/developer/static/media/econIndicators.c9f33a6f.png"
+  },
+  {
+    "revision": "93bbea6be6330055a76322b2c0ccaeac",
+    "url": "/developer/static/media/intelInside.93bbea6b.png"
+  },
+  {
+    "revision": "38fd25346519edae9e6c2101ea8097c1",
+    "url": "/developer/static/media/omgDir.38fd2534.png"
+  },
+  {
+    "revision": "7d691b318fceef38fb00389823caa39d",
+    "url": "/developer/static/media/omgDirHdr.7d691b31.png"
+  },
+  {
+    "revision": "a29d73284586808ba0980e4935363699",
+    "url": "/developer/static/media/retailNet2.a29d7328.png"
+  },
+  {
+    "revision": "b7929a38e14a68faf7934e2867b6ab87",
+    "url": "/developer/static/media/scIdea.b7929a38.png"
+  },
+  {
+    "revision": "5f10cee1a22c68a1b93a0b337591e0d1",
+    "url": "/developer/static/media/uModel.5f10cee1.png"
+  }
+]);
